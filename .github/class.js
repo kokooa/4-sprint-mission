413bf7b9709c956be80efee2af698d7b20ff1197
kokooa <ckdwns1379@gmail.com>
2025-07-22 16:21:17 +0900

    Add files via upload

---
+++ .github/class.js
@@ -0,0 +1,48 @@
+import { getProductList } from './product.js';
+
+class Product {
+    constructor(name, description, price, tags, images, favoriteCount) {
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.tags = tags;
+        this.images = images;
+        this.favoriteCount = favoriteCount;
+        favorite += 1;
+    }
+}
+
+class Article {
+    constructor(title, content, writer, likeCount) {
+        this.title = title;
+        this.content = content;
+        this.writer = writer;
+        this.likeCount = likeCount;
+        like += 1;
+    }
+}
+
+class ElectronicProduct extends Product{
+    constructor(data, manufacturer) {
+        super(data, favoriteCount)
+        this.manufacturer = manufacturer;
+    }
+}
+
+async function main() {
+    try {
+        const productDataList = await getProductList();
+        const products = [];
+
+        for (const productData of productDataList) {
+            if (productData.hashTags.includes('전자제품')) {
+                products.push(new ElectronicProduct(productData));
+            } else {
+                products.push(new Product(productData));
+            }
+            }
+            console.log(products);
+        } catch (error) {
+            console.error('❌ 상품 가져오기 실패:', error.message);
+    }
+}
